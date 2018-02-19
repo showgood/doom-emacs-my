77490556d65c80d69f312102a5800a3ba6a30995
showgood <showgood21@gmail.com>
2018-02-19 16:29:30 -0500

    update example for mac os

--- init.example.el
+++ init.example.el
@@ -68,9 +68,9 @@
        gist              ; interacting with github gists
        imenu             ; an imenu sidebar and searchable code index
        impatient-mode    ; show off code over HTTP
-      ;macos             ; MacOS-specific commands
+       macos             ; MacOS-specific commands
        make              ; run make tasks from Emacs
-       neotree           ; a project drawer, like NERDTree for vim
+       ;neotree           ; a project drawer, like NERDTree for vim
        password-store    ; password manager for nerds
        rotate-text       ; cycle region at point between text candidates
        term              ; terminals in Emacs
@@ -78,13 +78,13 @@
        upload            ; map local to remote projects via ssh/ftp
 
        :lang
-       assembly          ; assembly for fun or debugging
+       ;assembly          ; assembly for fun or debugging
        cc                ; C/C++/Obj-C madness
-       crystal           ; ruby at the speed of c
+       ;crystal           ; ruby at the speed of c
        clojure           ; java with a lisp
        csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
-       elixir            ; erlang done right
+       ;elixir            ; erlang done right
        elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        go                ; the hipster dialect
@@ -97,24 +97,27 @@
        ledger            ; an accounting system in Emacs
        lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
-       ocaml             ; an objective camel
+       ;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         +attach          ; custom attachment system
         +babel           ; running code in org
         +capture         ; org-capture in and outside of Emacs
         +export          ; centralized export system + more backends
-        +present         ; Emacs for presentations
+        ;+present         ; Emacs for presentations
         ;; TODO +publish
         )
+       ;; ============= NOTE: pdf module MUST be put after org module since the dependencies
+       pdf
+       ;; ==========================
        perl              ; write code no one else can comprehend
        php               ; make php less awful to work with
        plantuml          ; diagrams for confusing people more
-       purescript        ; javascript, but functional
+       ;purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
        rest              ; Emacs as a REST client
        ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       scala             ; java, but good
+       ;scala             ; java, but good
        sh                ; she sells (ba|z)sh shells on the C xor
        swift             ; who asked for emoji variables?
        typescript        ; javascript, but better
@@ -123,7 +126,7 @@
        ;; Applications are complex and opinionated modules that transform Emacs
        ;; toward a specific purpose. They may have additional dependencies and
        ;; should be loaded late.
-       :app
+       ;; :app
       ;email             ; emacs as an email client
       ;irc               ; how neckbeards socialize
       ;rss               ; emacs as an RSS reader
@@ -135,5 +138,5 @@
        ;; the defaults module. It contains a Spacemacs-inspired keybinding
        ;; scheme and additional ex commands for evil-mode. Use it as a reference
        ;; for your own.
-       :private default)
+       :private showgood)
 
