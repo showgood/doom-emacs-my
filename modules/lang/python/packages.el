dda10a73fbe4a5b7abdf2dc7cd15a43247a884d7
showgood <showgood21@gmail.com>
2018-02-19 12:53:41 -0500

    switch to elpy instead of anaconda-mode for python (#4)

--- modules/lang/python/packages.el
+++ modules/lang/python/packages.el
@@ -1,11 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/python/packages.el
 
-;; requires: python jedi setuptools
-
-(package! anaconda-mode)
-(package! nose)
 (package! pip-requirements)
-
-(when (featurep! :completion company)
-  (package! company-anaconda))
+(package! elpy)
