b7357fb6f022876580e6a506510e19c854124f58
showgood <showgood21@gmail.com>
2018-02-19 20:27:19 -0500

    version control enhancement (#7)

--- modules/feature/version-control/autoload.el
+++ modules/feature/version-control/autoload.el
@@ -68,3 +68,113 @@ putting them in `header-line-format' has better visibility."
                (propertize author 'face 'git-timemachine-minibuffer-author-face)
                (propertize sha-or-subject 'face 'git-timemachine-minibuffer-detail-face)
                date-full date-relative)))))
+
+;; https://github.com/fniessen/emacs-leuven/blob/master/emacs-leuven.el
+;;;###autoload
+(defun leuven-vc-dir-hide-up-to-date-and-unregistered ()
+    (interactive)
+    (vc-dir-hide-up-to-date)
+    (vc-dir-hide-unregistered))
+
+;;;###autoload
+(defun vc-dir-hide-unregistered ()
+    "Hide unregistered items from display."
+    (interactive)
+    (let ((crt (ewoc-nth vc-ewoc -1))
+          (first (ewoc-nth vc-ewoc 0)))
+      ;; Go over from the last item to the first and remove the unregistered
+      ;; files and directories with no child files.
+      (while (not (eq crt first))
+        (let* ((data (ewoc-data crt))
+               (dir (vc-dir-fileinfo->directory data))
+               (next (ewoc-next vc-ewoc crt))
+               (prev (ewoc-prev vc-ewoc crt))
+               ;; ewoc-delete does not work without this...
+               (inhibit-read-only t))
+          (when (or
+                 ;; Remove directories with no child files.
+                 (and dir
+                      (or
+                       ;; Nothing follows this directory.
+                       (not next)
+                       ;; Next item is a directory.
+                       (vc-dir-fileinfo->directory (ewoc-data next))))
+                 ;; Remove files in the unregistered state.
+                 (eq (vc-dir-fileinfo->state data) 'unregistered))
+            (ewoc-delete vc-ewoc crt))
+          (setq crt prev)))))
+
+;;;###autoload
+(defun vc-ediff-ignore-whitespace (historic &optional not-urgent)
+    "Ignore regions that differ in white space & line breaks only."
+    (interactive (list current-prefix-arg t))
+    (require 'ediff)
+    (let ((ediff-ignore-similar-regions t))
+      (call-interactively 'vc-ediff)))  ; XXX does not work yet!
+
+;; http://blog.binchen.org/posts/new-git-timemachine-ui-based-on-ivy-mode.html
+;;;###autoload
+(defun my-git-timemachine-show-selected-revision ()
+  "Show last (current) revision of file."
+  (interactive)
+  (let* ((collection (mapcar (lambda (rev)
+                    ;; re-shape list for the ivy-read
+                    (cons (concat (substring-no-properties (nth 0 rev) 0 7) "|" (nth 5 rev) "|" (nth 6 rev)) rev))
+                  (git-timemachine--revisions))))
+    (ivy-read "commits:"
+              collection
+              :action (lambda (rev)
+                        ;; compatible with ivy 9+ and ivy 8
+                        (unless (string-match-p "^[a-z0-9]*$" (car rev))
+                          (setq rev (cdr rev)))
+                        (git-timemachine-show-revision rev)))))
+
+;;;###autoload
+(defun my-git-timemachine ()
+  "Open git snapshot with the selected version.  Based on ivy-mode."
+  (interactive)
+  (unless (featurep 'git-timemachine)
+    (require 'git-timemachine))
+  (git-timemachine--start #'my-git-timemachine-show-selected-revision))
+
+;; http://blog.binchen.org/posts/enhance-emacs-git-gutter-with-ivy-mode.html
+;;;###autoload
+(defun my-reshape-git-gutter (gutter)
+  "Re-shape gutter for `ivy-read'."
+  (let* ((linenum-start (aref gutter 3))
+         (linenum-end (aref gutter 4))
+         (target-line "")
+         (target-linenum 1)
+         (tmp-line "")
+         (max-line-length 0))
+    (save-excursion
+      (while (<= linenum-start linenum-end)
+        (goto-line linenum-start)
+        (setq tmp-line (replace-regexp-in-string "^[ \t]*" ""
+                                                 (buffer-substring (line-beginning-position)
+                                                                   (line-end-position))))
+        (when (> (length tmp-line) max-line-length)
+          (setq target-linenum linenum-start)
+          (setq target-line tmp-line)
+          (setq max-line-length (length tmp-line)))
+
+        (setq linenum-start (1+ linenum-start))))
+    ;; build (key . linenum-start)
+    (cons (format "%s %d: %s"
+                  (if (eq 'deleted (aref gutter 1)) "-" "+")
+                  target-linenum target-line)
+          target-linenum)))
+
+;;;###autoload
+(defun my-goto-git-gutter ()
+  (interactive)
+  (if git-gutter:diffinfos
+      (ivy-read "git-gutters:"
+                (mapcar 'my-reshape-git-gutter git-gutter:diffinfos)
+                :action (lambda (e)
+                          ;; ivy9+ keep `(car e)'
+                          ;; ivy8- strip the `(car e)'
+                          ;; we handle both data structure
+                          (unless (numberp e) (setq e (cdr e)))
+                          (goto-line e)))
+    (message "NO git-gutters!")))
