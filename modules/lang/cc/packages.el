02d30a8f73796e9a3fddf178e92dd352d34730ee
showgood <showgood21@gmail.com>
2018-09-06 21:28:25 -0400

    minimal mode for cc mode module

--- modules/lang/cc/packages.el
+++ modules/lang/cc/packages.el
@@ -1,11 +1,14 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/cc/packages.el
 
-(package! cmake-mode)
-(package! demangle-mode)
-(package! disaster)
 (package! modern-cpp-font-lock)
 (package! clang-format)
 
 (when (featurep! :feature syntax-checker)
   (package! flycheck-irony))
+
+(unless (equal doom-mode "minimal")
+    (package! cmake-mode)
+    (package! demangle-mode)
+    (package! disaster)
+)
