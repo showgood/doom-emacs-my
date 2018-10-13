4298b3dc00511e32b7b1f576a03a45e051826452
showgood <showgood21@gmail.com>
2018-10-12 23:33:15 -0400

    [org-mode] always install org-plus-contrib

--- modules/lang/org/packages.el
+++ modules/lang/org/packages.el
@@ -1,13 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/org/packages.el
 
-(when (version< emacs-version "26.1")
-  ;; We want org 9.1.x, but the org packaged with Emacs 25.x and under is 8.x.
-  ;; The only secure (and reasonably trustworthy) source for this is via
-  ;; emacsmirror. Emacs 26+ comes with Org 9.1.4.
-  (package! org-plus-contrib))
-
-
+(package! org-plus-contrib)
 (package! org-bullets)
 (package! toc-org)
 (package! evil-org)
