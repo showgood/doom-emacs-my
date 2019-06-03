558b54ce65d8dce6eb1897c2d3c1918beba92818
showgood <showgood21@gmail.com>
2019-06-02 22:48:54 -0400

    update org-dayone

--- modules/my/mac/emacs-dayone2.el
+++ modules/my/mac/emacs-dayone2.el
@@ -1,4 +1,4 @@
-;;; my/mac/emacs-dayone2.el -*- lexical-binding: t; -*-
+;;; my/mac/org-dayone.el -*- lexical-binding: t; -*-
 
 ;; adopted from
 ;; https://github.com/ganmacs/emacs-dayone/blob/master/emacs-dayone.el
@@ -16,7 +16,7 @@
 (defvar emacs-dayone-succes-reg "^Created new entry with uuid:")
 
 ;;;###autoload
-(defun emacs-dayone2/save (entry-time tags &optional b e)
+(defun org-dayone/save(entry-time tags &optional b e)
   "Save current region to DayOne 2"
   (interactive "r")
   (let* ((start (if mark-active b (point-min)))
@@ -25,7 +25,7 @@
          (tmp-file (make-temp-file "emacs-dayone")))
     (with-temp-file tmp-file (insert contents))
     (message (number-to-string  start))
-    (emacs-dayone2/save-from-file tmp-file entry-time tags)))
+    (org-dayone/from-file tmp-file entry-time tags)))
 
 ;;;###autoload
 (defun emacs-dayone/export-to-md ()
@@ -34,15 +34,15 @@
 )
 
 ;;;###autoload
-(defun emacs-dayone2/save-as-md ()
+(defun org-dayone-as-md ()
   "Export current region as markdown and
    save to DayOne"
   (interactive)
-  (emacs-dayone2/save-from-file (emacs-dayone/export-to-md))
+  (org-dayone/from-file (emacs-dayone/export-to-md))
 )
 
 ;;;###autoload
-(defun emacs-dayone2/save-from-file (tmp-file entry-time tags)
+(defun org-dayone/from-file (tmp-file entry-time tags)
   "Save the content from tmp-file to DayOne"
   (interactive "r")
    (if (string-match emacs-dayone-succes-reg
@@ -54,7 +54,7 @@
 
 
 ;;;###autoload
-(defun emacs-dayone2/org-journal-export-to-dayone2 ()
+(defun org-dayone/export-file ()                   
   "take an org journal file and export all of the second level headings
    to DayOne 2 for Mac."
   (interactive)
@@ -62,39 +62,57 @@
         (lambda ()
           (let* ((title (nth 4 (org-heading-components)))
                  (tags (nth 5 (org-heading-components)))
-                (time (substring title 0 5))
-                (date (buffer-name))
-                (entry-time
-                 (format "%s-%s-%s %s:00"
-                         (substring date 0 4)
-                         (substring date 4 6)
-                         (substring date 6 8)
-                         time))
-               (export-time (org-entry-get (point) "EXPORT_TIME"))
-               (level (car (org-heading-components))))
+                 (time (substring title 0 5))
+                 (date (substring (buffer-name) 0 10))
+                 (entry-time (format "%s %s:00" date time))
+                 (export-time (org-entry-get (point) "EXPORT_TIME"))
+                 (level (car (org-heading-components))))
             (when (eq level 2)
-              (unless export-time
+              (if export-time
+                (message "title: %s has been exported before" title)
+
                 (org-set-property "EXPORT_TIME"
                                   (format "[%s]" entry-time))
+                (message "buffer-file-name: %s" (buffer-file-name))
+                (save-buffer)
                 (message "process %s, entry-time: %s" title entry-time)
                 (org-gfm-export-as-markdown nil t)
                 (switch-to-buffer "*Org GFM Export*")
                 (goto-char (point-min))
-                (while (search-forward-regexp "^#" nil t)
-                  (replace-match "##"))
-                (goto-char (point-min))
-                (insert (format "# %s\n\n" (substring title 6)))
-                (if tags
-                    (progn
-                        (setq tags (split-string tags ":" t))
-                        (add-to-list 'tags "org_journal")
-                    )
-                  (setq tags '("org_journal"))
-                )
+                (xah-clean-empty-lines)
+                (if (< (length (split-string (buffer-string) "\n" t)) 1)
+                    (message "skip empty title: %s" title)
+
+                    (while (search-forward-regexp "^#" nil t)
+                        (replace-match "##"))
+                    (goto-char (point-min))
+
+                    (if (> (length title) 5)
+                        (insert (format "# %s\n\n" (substring title 6)))
+                        (insert (format "# %s\n\n" title)))
+
+                    (if tags
+                        (progn
+                            (setq tags (split-string tags ":" t))
+                            (add-to-list 'tags "org_journal")
+                        )
+                    (setq tags '("org_journal")))
 
                 (setq tags (mapconcat 'identity tags " "))
-                (emacs-dayone2/save entry-time tags)
+                (org-dayone/save entry-time tags)
                 (kill-buffer "*Org GFM Export*"))))
-)))
+))))
 
-(provide 'emacs-dayone2)
+;;;###autoload
+(defun org-dayone/export-dir (dir)
+  (interactive)
+  (require 'find-lisp)
+  (mapc (lambda (f)
+          (message "process file: %s" f)
+          (find-file f)
+          (org-mode)
+          (org-dayone/export-file)
+        )
+        (find-lisp-find-files dir "\\.journal$")))
+
+(provide 'org-dayone)
