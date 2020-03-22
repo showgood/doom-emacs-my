bc2d32ba1e28c43f12e952a3e5d9bf76450690a9
showgood <showgood21@gmail.com>
2020-03-21 22:51:28 -0400

    use branch for [format-all] so we can disable formatting

--- modules/editor/format/packages.el
+++ modules/editor/format/packages.el
@@ -1,4 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/format/packages.el
 
-(package! format-all :pin "f57a2a8abb")
+;; (package! format-all :pin "f57a2a8abb")
+
+(package! format-all :recipe (:host github :repo "lassik/emacs-format-all-the-code" :branch "multi-formatter"))
