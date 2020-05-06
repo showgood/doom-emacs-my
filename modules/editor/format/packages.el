bd20e5b30051f6821c8ec258412a19254d6306e7
showgood <showgood21@gmail.com>
2020-05-05 23:01:43 -0400

    update format-all package

--- modules/editor/format/packages.el
+++ modules/editor/format/packages.el
@@ -1,4 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/format/packages.el
 
-(package! format-all :pin "47d862d40a")
+;; (package! format-all :pin "47d862d40a")
+
+;; need to use this branch since I want to disable it for certain directory
+(package! format-all :recipe (:host github :repo "lassik/emacs-format-all-the-code" :branch "multi-formatter"))
